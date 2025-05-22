e8745ef3941b9c2beb2a5f202a8c4e899aafa6de
Josef Bruseke <josefbruseke@gmail.com>
2025-05-21 22:05:56 -0300

    fix plugin error

--- app/(root)/calendar/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function CalendarLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="day" />  // Matches app/calendar/day.tsx
-    </Stack>
-  );
-}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,5 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function LoadingLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <Stack>
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-      <StatusBar style="auto" />
-    </Stack>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
